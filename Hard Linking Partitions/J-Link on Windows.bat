d6f6d3fa1317d157508d35c952c4168de1c1611f
Brady Lange <brady.lange111@gmail.com>
2018-06-08 18:03:51 -0500

    Fully functional program
    The batch file is now fully functional and can error handle most scenarios.

--- Hard Linking Partitions/J-Link on Windows.bat
+++ Hard Linking Partitions/J-Link on Windows.bat
@@ -24,12 +24,23 @@ set /p path="Drive Label:"
 echo Are you sure you want this partition label: %path%?
 set /p retry="Enter 'Y' or 'N'.
 if /i "%retry%" == "Y" (
-  echo Successfully registered your main partition! & echo.
+    echo Successfully registered your main partition! & echo.
+    pause
 ) else if /i "%retry%" == "N" (
-  GOTO PartitionLabels
+    GOTO PartitionLabels
 ) else (
-  set UserError = "Partitions"
-  GOTO UserErrorHandling
+    set UserError = "Partitions"
+    GOTO UserErrorHandling
+)
+echo ONLY press 'F' if you have already ran this program and want to finish the programs job!
+set finish="Enter 'C' to continue or 'F' if you want to FINISH manipulating the 'Program Data' directory: "
+if /i "%finish%" == "F" (
+    GOTO CleanLinks
+) else if /i "%finish%" == "C"
+    echo.
+) else
+    set UserError = "Finish"
+    GOTO UserErrorHandling
 )
 
 echo Thank you! Now, what partition would you like these folders to be linked to?
@@ -38,13 +49,13 @@ set /p linkpath="Drive Label:"
 echo Are you sure you want this partition label: %linkpath%?
 set /p retry="Enter 'Y' or 'N'.
 if /i "%retry%" == "Y" (
-  echo Successfully registered your secondary partition! & echo.
-  timeout /t 2
+    echo Successfully registered your secondary partition! & echo.
+    pause
 ) else if /i "%retry%" == "N" (
-  GOTO PartitionLabels
+    GOTO PartitionLabels
 ) else (
-  set UserError = "Partitions"
-  GOTO UserErrorHandling
+    set UserError = "Partitions"
+    GOTO UserErrorHandling
 )
 
 :: User Welcome Script
@@ -52,24 +63,25 @@ if /i "%retry%" == "Y" (
 echo Welcome! 
 set /p response=Enter 'C' to continue, 'E' to leave, or 'P' to alter your partition labels:
 if /i "%response%" == "C" (
-  echo Thank you. Now moving to some verification. & echo.
-  pause
-  cls
-  GoTo WarningScript
+    echo Thank you. Now moving to some verification. & echo.
+    pause
+    cls
+    GoTo WarningScript
 ) else if "%response%" == "" (
-  set UserError = "BlankWelcome"
-  GOTO UserErrorHandling
+    set UserError = "BlankWelcome"
+    GOTO UserErrorHandling
 ) else if /i "%response%" == "E" (
-  echo Thank you for using Brady Lange's J-Link Program.
-  echo Have a wonderful rest of your day!
-  timeout 3
-  exit
+    :ExitProgram
+    cls
+    echo Thank you for using Brady Lange's J-Link Program.
+    echo Have a wonderful rest of your day!
+    timeout 3
+    exit
 ) else if /i "%response%" == "P" (
-  GOTO PartitionLabels
-
+    GOTO PartitionLabels
 ) else (
-  set UserError = "Welcome"
-  GOTO UserErrorHandling
+    set UserError = "Welcome"
+    GOTO UserErrorHandling
 )
 
 :: User Interaction for Verification:
@@ -80,118 +92,129 @@ echo For safety make sure you use this batch file on a clean install of a Window
 echo If used with a current operating system, it could cause system instability. & echo.
 set /p response=Enter 'C' to continue or 'E' to cancel:  
 if /i "%response%" == "C" (
-  echo Your wish is my command!
-  timeout 1
-  GoTo RoboCopyDirectories
+    echo Your wish is my command!
+    GoTo RoboCopyDirectories
 ) else if "%response%" == "" (
-  set UserError = "BlankWarning"
-  GOTO UserErrorHandling
+    set UserError = "BlankWarning"
+    GOTO UserErrorHandling
 ) else if /i "%response%" == "E" (
-  GOTO Home
+    GOTO Home
 ) else (
-  set UserError = "Warning"
-  GOTO UserErrorHandling
+    set UserError = "Warning"
+    GOTO UserErrorHandling
 )
 cls
 
 :: Catch user input error
 :UserErrorHandling
 if "%UserError%" == "Welcome" (
-  echo Error: You must ONLY use the key strokes 'c' or 'e' NOT %response%
-  echo 'c' = Continue with the program.
-  echo 'e' = Exit the program.
-  timeout 10
+    echo Error: You must ONLY use the key strokes 'C' or 'E' NOT %response%
+    echo 'C' = Continue with the program.
+    echo 'E' = Exit the program.
+    pause
   GOTO Home
 ) else if "%UserError%" == "Warning" (
-  echo Error: You must ONLY use the key strokes 'c' or 'e' NOT %response%
-  echo 'c' = Continue with the program.
-  echo 'e' = Cancel this step and go to home.
-  timeout 10
-  GOTO WarningScript
+    echo Error: You must ONLY use the key strokes 'C' or 'E' NOT %response%
+    echo 'C' = Continue with the program.
+    echo 'E' = Cancel this step and go to home.
+    pause
+    GOTO WarningScript
 ) else if "%UserError%" == "BlankWelcome" (
-  echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
-  echo Your options are: 
-  echo 'c' = Continue with the program.
-  echo 'e' = Cancel this step and go to home.
-  timeout 10
-  GOTO Home
+    echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
+    echo Your options are: 
+    echo 'C' = Continue with the program.
+    echo 'E' = Cancel this step and go to home.
+    pause
+    GOTO Home
 ) else if "%UserError%" == "BlankWarning" (
-  echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
-  echo Your options are: 
-  echo 'c' = Continue with the program.
-  echo 'e' = Cancel this step and go to home.
-  echo Please try again.
-  timeout 10
-  GOTO WarningScript
+    echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
+    echo Your options are: 
+    echo 'C' = Continue with the program.
+    echo 'E' = Cancel this step and go to home.
+    echo Please try again.
+    pause
+    GOTO WarningScript
 ) else if "%UserError%" == "Partitions" (
-  echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
-  echo Your options are: 
-  echo 'Y' = Yes.
-  echo 'N' = No.
-  timeout 5
-  GOTO PartitionLabels
-)
+    echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
+    echo Your options are: 
+    echo 'Y' = Yes.
+    echo 'N' = No.
+    pause
+    GOTO PartitionLabels
+) else if "%UserError%" == "Finish" (
+    echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
+    echo Your options are: 
+    echo 'C' = Continue (first time running program)
+    echo 'F' = Finish program (already have completed program steps)
+    pause
+    GOTO PartitionLabels
+)    
 
 :RoboCopyDirectories
-rem -- Note: Press shift and F10 to bring up the CMD prompt upon installing Windows 
-rem -- The robocopy command copys the folders from the system C: partition and pastes them onto the D: partition 
-echo Copying your directories to 
-timeout 1
-robocopy "C:\Users" "D:\Users" /E /COPYALL /XJ
-robocopy "C:\Program Files" "D:\Program Files" /E /COPYALL /XJ
-robocopy "C:\Program Files (x86)" "D:\Program Files (x86)" /E /COPYALL /XJ
-robocopy "C:\ProgramData" "D:\ProgramData" /E /COPYALL /XJ
+rem -- Note: Press shift and F10 to bring up the CMD prompt upon installing Windows if copying and pasting this script
+rem -- The robocopy command copys the folders from the system %linkpath%: partition and pastes them onto the %path%: partition 
+cls
+echo Copying your directories from %linkpath% to %path%...
+pause
+robocopy "%linkpath%:\Users" "%path%:\Users" /E /COPYALL /XJ
+robocopy "%linkpath%:\Program Files" "%path%:\Program Files" /E /COPYALL /XJ
+robocopy "%linkpath%:\Program Files (x86)" "%path%:\Program Files (x86)" /E /COPYALL /XJ
+robocopy "%linkpath%:\ProgramData" "%path%:\ProgramData" /E /COPYALL /XJ
 
-rem -- The rmdir command removes the original directory in the C: partition since it will no longer be needed
+rem -- The rmdir command removes the original directory in the %linkpath%: partition since it will no longer be needed
 :: /S = Recursively drill down into directories
 :RemoveDirectories
-echo CLEANING up old directories!
-timeout 1
-rmdir "C:\Users" /S /Q
-rmdir "C:\Program Files" /S /Q
-rmdir "C:\Program Files (x86)" /S /Q
+cls
+echo CLEANING up old directories off of the %linkpath%: drive...
+pause
+rmdir "%linkpath%:\Users" /S /Q
+rmdir "%linkpath%:\Program Files" /S /Q
+rmdir "%linkpath%:\Program Files (x86)" /S /Q
 rem -- Upon trying to remove ProgramData 4 files will not be deleted because of "Access Denied"
 rem -- The partition thinks these files are being used currently
 rem -- You will still need to remove the rest of ProgramData directory as it will not fully be removed 
-rmdir "C:\ProgramData" /S /Q
-echo An ERROR will occur on some of the 'C:\ProgramData' files
+rmdir "%linkpath%:\ProgramData" /S /Q
+echo An ERROR will occur on some of the '%linkpath%:\ProgramData' files
 echo No worries! This is being taken care of right away
-timeout 5
+pause
 
 :MakeJLinks
-rem -- The mklink (Make Link) with the /J property creates a hard link from the D: partition to the C: partition
+cls
+rem -- The mklink (Make Link) with the /J property creates a hard link from the %path%: partition to the %linkpath%: partition
 rem -- Now the C: (system partition) thinks it has its proper system directories
-rem -- Don't J-Link the ProgramData directory as it hasn't been fully removed from the C: partition 
+rem -- Don't J-Link the ProgramData directory as it hasn't been fully removed from the %linkpath%: partition 
 echo CREATING J Link Directories!
-timeout 1
-mklink /J "C:\Users" "D:\Users"
-mklink /J "C:\Program Files" "D:\Program Files"
-mklink /J "C:\Program Files (x86)" "D:\Program Files (x86)"
-timeout 10
-exit
+pause
+mklink /J "%linkpath%:\Users" "%path%:\Users"
+mklink /J "%linkpath%:\Program Files" "%path%:\Program Files"
+mklink /J "%linkpath%:\Program Files (x86)" "%path%:\Program Files (x86)"
 
-rem -- Open up the Registry Editor (regedit) to register the partition labels to correct label of D:
+rem -- Open up the Registry Editor (regedit) to register the partition labels to correct label of %path%:
 echo Now opening Registry Editor (REGEDIT)...
-timeout 3
+pause
 regedit 
-rem -- Navigate the folders
-rem -- HKEY_LOCAL_MACHINE\SOFTWARE\MICROSOFT\WINDOWS\CurrentVersion
-rem -- In the CurrentVersion directory change all of the partition labels to D: instead of C:
-rem -- HKEY_LOCAL_MACHINE\SOFTWARE\MICROSOFT\WINDOWS NT\CurrentVersion\ProfileList
-rem -- In the ProfileList directory change all of the partition labels to D: instead of C:
-timeout 10
+echo -- Navigate the folders:
+echo -- HKEY_LOCAL_MACHINE\SOFTWARE\MICROSOFT\WINDOWS\CurrentVersion
+echo -- In the CurrentVersion directory change all of the partition labels to %path%: instead of %linkpath%:
+echo -- HKEY_LOCAL_MACHINE\SOFTWARE\MICROSOFT\WINDOWS NT\CurrentVersion\ProfileList
+echo -- In the ProfileList directory change all of the partition labels to %path%: instead of %linkpath%: & echo.
+echo -- After, done complete installing of the operating system.
+pause
 exit
 
 :CleanLinks
-rem -- Complete installation of Windows then open the command prompt and remove the rest of the ProgramData directory from the C: partition
-rmdir "C:\ProgramData" /S /Q
+echo CLEANING the 'ProgramData' directory and junction linking it... & echo.
+pause
+rem -- Complete installation of Windows then open the command prompt and remove the rest of the ProgramData directory from the %linkpath%: partition
+rmdir "%linkpath%:\ProgramData" /S /Q
 rem -- Finally, open the command prompt and create the junction for ProgramData with this command:
-mklink /J "C:\ProgramData" "D:\ProgramData"
+mklink /J "%linkpath%:\ProgramData" "%path%:\ProgramData"
 
 :TestForSuccess
 rem -- Now test to see if you have correctly configured your junction directories
 rem -- Directory to Search
-set directory = C:\
+echo Finding junction directories that you have created... & echo.
+set directory = %linkpath%:\
 
 rem -- The directory that will be searched
 echo Directory Search: %directory% & echo.
@@ -210,7 +233,8 @@ rem -- Find JUNCTIONS in directory
 dir "%directory%" | find "<JUNCTION>" 
 if %ERRORLEVEL% EQU 0 echo This is a Directory Junction
 
-timeout 10
+echo You have SUCCESSFULLY junctioned the system folders 
+echo from the %path%: partition to the %linkpath%: partition (system partition)
+pause
+GOTO ExitProgram
 exit
-
-rem -- Successfully junctioned the system folders from the D: partition to the C: partition (system partition)
