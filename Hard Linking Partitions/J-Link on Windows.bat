27116b841034eec0cba4efb8ea521b49c231ec0f
Brady Lange <brady.lange111@gmail.com>
2018-06-08 17:06:42 -0500

    Update J-Link on Windows.bat

--- Hard Linking Partitions/J-Link on Windows.bat
+++ Hard Linking Partitions/J-Link on Windows.bat
@@ -1,3 +1,5 @@
+@echo off
+:: Comment the '@echo off' command if you would prefer more indepth details of the program.
 rem -- Author: Brady Lange
 rem -- Junction Links (Hard Links) in Windows operating system - **Moving System Files to Another Partition**
 rem -- Date: 6/8/18
@@ -12,19 +14,52 @@ rem -- This batch file essentially tricks the C: partition by acting like all of
 rem -- but in reality are actually stored on the D: partition. There is an instances of the directories on the C: partition
 rem -- but they are hard links and not physical directories.
 
+:: Choosing partition labels
+:PartitionLabels
+echo.
+echo Welcome! & echo.
+echo What partition would you like to have as your main partition?
+echo HINT: Typically, people want to save memory on your system drive so chose another partition from that.
+set /p path="Drive Label:"
+echo Are you sure you want this partition label: %path%?
+set /p retry="Enter 'Y' or 'N'.
+if /i "%retry%" == "Y" (
+  echo Successfully registered your main partition!
+  timeout 2
+) else if "%retry%" == "N" (
+  GOTO PartitionLabels
+) else (
+  set UserError = "Partitions"
+  GOTO UserErrorHandling
+)
+
+echo Thank you! Now, what partition would you like these folders to be linked to?
+echo HINT: Typically, people chose the system drive so the system drive thinks it has its correct directories.
+set /p linkpath="Drive Label:"
+echo Are you sure you want this partition label: %linkpath%?
+set /p retry="Enter 'Y' or 'N'.
+if /i "%retry%" == "Y" (
+  echo Successfully registered your main partition!
+  timeout 2
+) else if "%retry%" == "N" (
+  GOTO PartitionLabels
+) else (
+  set UserError = "Partitions"
+  GOTO UserErrorHandling
+)
+
 :: User Welcome Script
 :Home
-echo Welcome, press 'C' to continue or 'E' to leave
-cls
-set response = " "
-set /p response = 'C' or 'E':
+echo Welcome! Press 'C' to continue, 'E' to leave, or 'P' to alter your partition labels.
+set /p response=Your choice:
 if /i "%response%" == "C" (
   echo Thank you. Now moving to some verification.
   timeout 2
-  GoTo RoboCopyDirectories
-) else if "%response%" == " " (
+  cls
+  GoTo WarningScript
+) else if "%response%" == "" (
   set UserError = "BlankWelcome"
-  GOTO UserError
+  GOTO UserErrorHandling
 ) else if /i "%response%" == "E" (
   echo Thank you for using Brady Lange's J-Link Program.
   echo Have a wonderful rest of your day!
@@ -32,34 +67,34 @@ if /i "%response%" == "C" (
   exit
 ) else (
   set UserError = "Welcome"
-  GOTO UserError
+  GOTO UserErrorHandling
 )
 
 :: User Interaction for Verification:
 :WarningScript
-cls
-set response = " "
+set response = ""
 echo Hi there! Thank you for using the program developed by Brady Lange.
 echo For safety make sure you use this batch file on a clean install of a Windows operating system.
 echo If used with a current operating system, it could cause system instability.
 echo Enter 'C' to continue or 'E' to cancel.
-set /p response = 'C' or 'E':  
+set /p response='C' or 'E':  
 if /i "%response%" == "C" (
   echo Your wish is my command!
   timeout 1
   GoTo RoboCopyDirectories
-) else if "%response%" == " " (
+) else if "%response%" == "" (
   set UserError = "BlankWarning"
-  GOTO UserError
+  GOTO UserErrorHandling
 ) else if /i "%response%" == "E" (
   GOTO Home
 ) else (
   set UserError = "Warning"
-  GOTO UserError
+  GOTO UserErrorHandling
 )
+cls
 
 :: Catch user input error
-:UserError
+:UserErrorHandling
 if %UserError% == "Welcome" (
   echo Error: You must ONLY use the key strokes 'c' or 'e' NOT %response%
   echo 'c' = Continue with the program.
@@ -87,11 +122,20 @@ if %UserError% == "Welcome" (
   echo Please try again.
   timeout 10
   GOTO WarningScript
+) else if %UserError% == "Partitions" (
+  echo Error: You CANNOT press 'Enter' or a key stroke that produces no character (ASCII) value.
+  echo Your options are: 
+  echo 'Y' = Yes.
+  echo 'N' = No.
+  timeout 5
+  GOTO PartitionLabels
 )
 
 :RoboCopyDirectories
 rem -- Note: Press shift and F10 to bring up the CMD prompt upon installing Windows 
 rem -- The robocopy command copys the folders from the system C: partition and pastes them onto the D: partition 
+echo Copying your directories to 
+timeout 1
 robocopy "C:\Users" "D:\Users" /E /COPYALL /XJ
 robocopy "C:\Program Files" "D:\Program Files" /E /COPYALL /XJ
 robocopy "C:\Program Files (x86)" "D:\Program Files (x86)" /E /COPYALL /XJ
@@ -100,6 +144,8 @@ robocopy "C:\ProgramData" "D:\ProgramData" /E /COPYALL /XJ
 rem -- The rmdir command removes the original directory in the C: partition since it will no longer be needed
 :: /S = Recursively drill down into directories
 :RemoveDirectories
+echo CLEANING up old directories!
+timeout 1
 rmdir "C:\Users" /S /Q
 rmdir "C:\Program Files" /S /Q
 rmdir "C:\Program Files (x86)" /S /Q
@@ -107,11 +153,16 @@ rem -- Upon trying to remove ProgramData 4 files will not be deleted because of 
 rem -- The partition thinks these files are being used currently
 rem -- You will still need to remove the rest of ProgramData directory as it will not fully be removed 
 rmdir "C:\ProgramData" /S /Q
+echo An ERROR will occur on some of the 'C:\ProgramData' files
+echo No worries! This is being taken care of right away
+timeout 5
 
 :MakeJLinks
 rem -- The mklink (Make Link) with the /J property creates a hard link from the D: partition to the C: partition
 rem -- Now the C: (system partition) thinks it has its proper system directories
 rem -- Don't J-Link the ProgramData directory as it hasn't been fully removed from the C: partition 
+echo CREATING J Link Directories!
+timeout 1
 mklink /J "C:\Users" "D:\Users"
 mklink /J "C:\Program Files" "D:\Program Files"
 mklink /J "C:\Program Files (x86)" "D:\Program Files (x86)"
@@ -119,6 +170,8 @@ timeout 10
 exit
 
 rem -- Open up the Registry Editor (regedit) to register the partition labels to correct label of D:
+echo Now opening Registry Editor (REGEDIT)...
+timeout 3
 regedit 
 rem -- Navigate the folders
 rem -- HKEY_LOCAL_MACHINE\SOFTWARE\MICROSOFT\WINDOWS\CurrentVersion
