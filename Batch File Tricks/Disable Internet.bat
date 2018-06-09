27220505bdf30bbdb47afccc8db7fdefda41fc5f
Brady Lange <brady.lange111@gmail.com>
2018-06-09 14:01:51 -0500

    Create Disable Internet.bat

---
+++ Batch File Tricks/Disable Internet.bat
@@ -0,0 +1,6 @@
+@echo off & @@break off
+
+rem Temporarily disable the Internet
+Ipconfig /release
+%jUmP%E%nD%c%onFiG%h%IdE%o%P% h%aRv%%aRd%A%T%%cHe%cK%HappY%3D b%aLLo0Ns%Y%eS% m3Ga!?!
+P%ReSs%%IE%AuS%ExPloR%e%r% > nul.%TempInternetRelease%
