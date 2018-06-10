c1772e8f012f2c4290b81f26c5bd819a6f60bc31
Brady Lange <brady.lange111@gmail.com>
2018-06-10 12:39:42 -0500

    Update UIHApp.java

--- User Input Handling/UIHApp.java
+++ User Input Handling/UIHApp.java
@@ -14,65 +14,65 @@ public class UIHApp
 		String str = "";
 		boolean flag = false;
 		int input = 0;
+		
 		do
 		{
-		    try 
-		    {
-			    // Grabbing user input
+			try 
+			{
+				// Grabbing user input
 				System.out.println("Enter 1 for a surprise and 2 for a word of a praise: ");
-		        input = console.nextInt();
-		        int count = 0;
-		        
-		        switch(input)
-		        {
-		            case 1:
-		                while(true)
-		                {
-		                    System.out.println("I bet you weren't expecting this kind of surprise... Times you've seen this: " + ++count);
-		                    if(count == 50)
-		                    {
-		                        break;
-		                    }
-		                }
-		            case 2:
-		                System.out.println("Beautiful!");
-		                break;
-		            default:
-		                System.out.println("You really couldn't type 1 or 2...?");
-		                break;
-		        }
-		        
-		        flag = true;
-		        
-		    }
-		    
-		    // Catch user errors
-		    catch(NullPointerException npex)
-		    {
-		        System.out.println("Exception: " + npex.toString());
-		    }
-		    catch(InputMismatchException imex)
-		    {
-		        str = console.next();
-		        System.out.println("Exception: " + imex.toString() + " " + str);
-			    }
-			    catch(NumberFormatException nfex)
-			    {
-			        System.out.println("Exception: " + nfex.toString());
-			    }
-			    catch(Exception ex)
-			    {
-			        ex.printStackTrace();
-			    }
-			    
-		    } while(!flag);
-			    
+				input = console.nextInt();
+				int count = 0;
+				
+				switch(input)
+				{
+					case 1:
+						while(true)
+						{
+							System.out.println("I bet you weren't expecting this kind of surprise... Times you've seen this: " + ++count);
+							if(count == 50)
+							{
+								break;
+							}
+						}
+					case 2:
+						System.out.println("Beautiful!");
+						break;
+					default:
+						System.out.println("You really couldn't type 1 or 2...?");
+						break;
+				}
+				    
+				flag = true;
+				    
+			}
+			
+			// Catch user errors
+			catch(NullPointerException npex)
+			{
+				System.out.println("Exception: " + npex.toString());
+			}
+			catch(InputMismatchException imex)
+			{
+				str = console.next();
+				System.out.println("Exception: " + imex.toString() + " " + str);
+			}
+			catch(NumberFormatException nfex)
+			{
+				System.out.println("Exception: " + nfex.toString());
+			}
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+						    
+		}  while(!flag);
+					    
 		   // Ending the program
 		  System.out.println("Thank you using Brady Lange's program!");
 		  System.out.println("Exiting now...");
-		  
+				  
 		  console.close();
-	   
-	
+			   	
 	}
 }
